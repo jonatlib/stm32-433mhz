cfa44377309ece11a7fef12e9cb1651a5a279374
Libor Jonat <jonat.libor@gmail.com>
2023-02-05 21:01:55 +0100

    Work on packet builder and codec

--- workspaces/network/src/packet_builder.rs
+++ workspaces/network/src/packet_builder.rs
@@ -4,6 +4,10 @@ use crate::Address;
 pub struct PacketBuilder<'a, I> {
     address: &'a Address,
 
+    first_element: bool,
+    self_contained: bool,
+    prev_element: Option<&'a u8>,
+
     payload: I,
 }
 
@@ -11,8 +15,14 @@ impl<'a, I> PacketBuilder<'a, I>
 where
     I: Iterator<Item = &'a u8>,
 {
-    pub fn new(address: &'a Address, payload: I) -> Self {
-        Self { address, payload }
+    pub fn new(address: &'a Address, mut payload: I) -> Self {
+        Self {
+            address,
+            first_element: true,
+            self_contained: true,
+            prev_element: payload.next(),
+            payload,
+        }
     }
 }
 
@@ -23,15 +33,34 @@ where
     type Item = Packet32;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let byte_0 = *(self.payload.next()?);
+        let byte_0 = *self.prev_element?;
         let byte_1 = self.payload.next().map(|v| *v);
+        let byte_2 = self.payload.next();
+
+        self.prev_element = byte_2;
+        if byte_2.is_some() {
+            self.self_contained = false;
+        }
+
+        let kind = if self.self_contained {
+            PacketKind::SelfContained
+        } else {
+            if self.first_element {
+                self.first_element = false;
+                PacketKind::Start
+            } else if byte_2.is_some() {
+                PacketKind::Continue
+            } else {
+                PacketKind::End
+            }
+        };
 
         let payload =
             u16::from_be_bytes([byte_0, if let Some(byte) = byte_1 { byte } else { 0x00 }]);
 
         Some(
             Packet32::new()
-                .with_kind(PacketKind::SelfContained) // FIXME this is wrong
+                .with_kind(kind) // FIXME this is wrong
                 .with_sequence_number(0) // FIXME count sequence numbers (even outside of this builder)
                 .with_source_address(self.address.source_address)
                 .with_destination_address(self.address.destination_address)
