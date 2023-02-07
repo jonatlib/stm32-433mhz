5707ae6dbdb42f2da01d6de812a0cb9619ab7506
Libor Jonat <libor.jonat@quantlane.com>
2023-02-07 13:44:18 +0100

    Working on HW layer

--- src/hardware/machine/nucleo_stm32l053r8.rs
+++ src/hardware/machine/nucleo_stm32l053r8.rs
@@ -2,7 +2,7 @@ use crate::hardware::HardwareSetup;
 
 pub struct Hardware {}
 
-impl HardwareSetup for Hardware {}
+// impl HardwareSetup for Hardware {}
 
 pub mod io {
     use embassy_stm32::exti::ExtiInput;
