1d92621f741a42e5f391973791ed696db1b83e6b
Libor Jonat <libor.jonat@quantlane.com>
2023-02-08 16:18:11 +0100

    Working on sequence numbers tests

--- workspaces/sequence_number/src/lib.rs
+++ workspaces/sequence_number/src/lib.rs
@@ -46,15 +46,7 @@ impl<const MODULO: u8> SequenceNumber<MODULO> {
     /// sequence numbers if they were ordered the way that
     /// `other` comes first and `self` comes later.
     pub fn negative_distance(&self, other: &Self) -> u8 {
-        if self == other {
-            return 0;
-        }
-
-        if other.value() < self.value() {
-            self.value() - other.value()
-        } else {
-            (MODULO - other.value()) + self.value()
-        }
+        other.positive_distance(self)
     }
 
     pub fn min_distance(&self, other: &Self) -> u8 {
@@ -99,6 +91,8 @@ impl<const MODULO: u8> PartialEq for SequenceNumber<MODULO> {
     }
 }
 
+impl<const MODULO: u8> Eq for SequenceNumber<MODULO> {}
+
 impl<const MODULO: u8> PartialOrd for SequenceNumber<MODULO> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         if self == other {
@@ -108,5 +102,144 @@ impl<const MODULO: u8> PartialOrd for SequenceNumber<MODULO> {
         } else {
             Some(Ordering::Less)
         }
+    }
+}
+
+impl<const MODULO: u8> Ord for SequenceNumber<MODULO> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.partial_cmp(other)
+            .expect("Ordering should always exists.")
+    }
+}
+
+#[cfg(test)]
+#[macro_use]
+extern crate std;
+#[cfg(test)]
+mod test {
+    use std::vec::Vec;
+
+    use super::SequenceNumber;
+    type SN = SequenceNumber<8>;
+
+    #[test]
+    fn test_simple_comparison() {
+        let a = SN::new(4);
+        let b = SN::new(5);
+
+        assert!(a < b);
+        assert!(b > a);
+
+        let a = SN::new(8);
+        let b = SN::new(0);
+
+        assert_eq!(a, b);
+    }
+
+    #[test]
+    fn test_complex_comparison() {
+        let a = SN::new(7);
+        let b = SN::new(0);
+
+        assert_eq!(
+            a.positive_distance(&b),
+            1,
+            "From 7mod8 to 0mod8 it is one step forward"
+        );
+        assert_eq!(
+            a.negative_distance(&b),
+            7,
+            "From 7mod8 to 0mod8 backwards it is 7steps back."
+        );
+
+        // FIXME
+        println!("{}", a.positive_distance(&b)); // This should be 1
+        println!("{}", a.negative_distance(&b)); //
+
+        // assert!(a > b);
+        assert!(b > a);
+    }
+
+    #[test]
+    fn test_ordering_simple() {
+        let mut numbers: Vec<_> = (0..8u8).into_iter().map(SN::new).collect();
+        numbers.sort();
+        let result: Vec<u8> = numbers.iter().map(|v| v.value()).collect();
+        assert_eq!(result, vec![0, 1, 2, 3, 4, 5, 6, 7]);
+    }
+
+    #[test]
+    fn test_ordering_random() {
+        let mut numbers: Vec<_> = vec![2, 5, 7, 3, 1, 4, 6, 0]
+            .into_iter()
+            .map(SN::new)
+            .collect();
+        numbers.sort();
+        let result: Vec<u8> = numbers.iter().map(|v| v.value()).collect();
+
+        assert_eq!(result, vec![0, 1, 2, 3, 4, 5, 6, 7]);
+    }
+
+    #[test]
+    fn test_ordering_complex() {
+        let mut numbers: Vec<_> = vec![5, 6, 3, 4, 7, 0, 2, 1]
+            .into_iter()
+            .map(SN::new)
+            .collect();
+        numbers.sort();
+        let result: Vec<u8> = numbers.iter().map(|v| v.value()).collect();
+        assert_eq!(result, vec![3, 4, 5, 6, 7, 0, 1, 2]);
+    }
+
+    #[test]
+    fn test_positive_distance() {
+        let base = SN::new(0);
+
+        let positive_distances: Vec<_> = (0..8u8)
+            .into_iter()
+            .map(SN::new)
+            .map(|v| v.positive_distance(&base))
+            .collect();
+
+        assert_eq!(positive_distances, vec![0, 7, 6, 5, 4, 3, 2, 1])
+    }
+
+    #[test]
+    fn test_positive_distance_reverse() {
+        let base = SN::new(0);
+
+        let positive_distances: Vec<_> = (0..8u8)
+            .into_iter()
+            .map(SN::new)
+            .map(|v| base.positive_distance(&v))
+            .collect();
+
+        assert_eq!(positive_distances, vec![0, 1, 2, 3, 4, 5, 6, 7])
+    }
+
+    #[test]
+    fn test_negative_distance() {
+        let base = SN::new(0);
+
+        let negative_distances: Vec<_> = (0..8u8)
+            .into_iter()
+            .map(SN::new)
+            .map(|v| v.negative_distance(&base))
+            .collect();
+
+        assert_eq!(negative_distances, vec![0, 1, 2, 3, 4, 5, 6, 7])
+    }
+
+    #[test]
+    fn test_negative_distance_reverse() {
+        let base = SN::new(0);
+
+        let negative_distances: Vec<_> = (0..8u8)
+            .into_iter()
+            .map(SN::new)
+            .map(|v| base.negative_distance(&v))
+            .collect();
+
+        assert_eq!(negative_distances, vec![0, 7, 6, 5, 4, 3, 2, 1])
     }
 }
