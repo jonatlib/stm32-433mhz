85a36f2f7dddc60005ad323978bae6e1004b2207
Libor Jonat <jonat.libor@gmail.com>
2023-02-20 19:59:47 +0100

    Reed solomon codec

--- workspaces/codec/src/reed_solomon.rs
+++ workspaces/codec/src/reed_solomon.rs
@@ -2,8 +2,19 @@ use crate::{Codec, CodecSize};
 
 use reed_solomon::{Decoder, Encoder};
 
-#[derive(Default)]
-pub struct ReedSolomon<const ECC_LEN: usize> {}
+pub struct ReedSolomon<const ECC_LEN: usize> {
+    encoder: Encoder,
+    decoder: Decoder,
+}
+
+impl<const ECC_LEN: usize> Default for ReedSolomon<ECC_LEN> {
+    fn default() -> Self {
+        Self {
+            encoder: Encoder::new(ECC_LEN),
+            decoder: Decoder::new(ECC_LEN),
+        }
+    }
+}
 
 impl<const ECC_LEN: usize> Codec for ReedSolomon<ECC_LEN>
 where
@@ -13,15 +24,18 @@ where
     type Decoded<'a> = impl Iterator<Item = u8> + 'a;
 
     fn encode<'a>(&self, payload: &'a [u8]) -> Self::Encoded<'a> {
-        let encoder = Encoder::new(ECC_LEN);
-        let encoded: heapless::Vec<_, { ReedSolomon::<{ ECC_LEN }>::get_encode_const_size(4) }> =
-            encoder.encode(payload).into_iter().copied().collect();
+        // let encoded: heapless::Vec<_, { ReedSolomon::<{ ECC_LEN }>::get_encode_const_size(4) }> =
+        let encoded: heapless::Vec<_, { Self::get_encode_const_size(4) }> =
+            self.encoder.encode(payload).into_iter().copied().collect();
 
         encoded.into_iter()
     }
 
     fn decode<'a>(&self, payload: &'a [u8]) -> Self::Decoded<'a> {
-        payload.iter().copied()
+        //FIXME decode (maybe even encode) can return an error
+        let decode_buffer = self.decoder.correct(payload, None).expect("TODO");
+        let decoded: heapless::Vec<_, 4> = decode_buffer.into_iter().copied().collect();
+        decoded.into_iter()
     }
 
     fn get_encode_size(payload_size: usize) -> usize {
