22b15b47eea2c89baadc01c475a67882b7368197
Libor Jonat <libor.jonat@quantlane.com>
2023-03-09 14:55:32 +0100

    Global allocator

--- workspaces/external_memory/src/lib.rs
+++ workspaces/external_memory/src/lib.rs
@@ -8,6 +8,27 @@
 #[macro_use]
 extern crate std;
 
+pub type GlobalAllocator = &'static (dyn allocator::Allocator + Sync);
+
+/// Global variable holding the global allocator used in cases where you can't pass it
+static _GLOBAL_DEFAULT_EXTERNAL_ALLOCATOR: AllocatorHandler =
+    AllocatorHandler(core::cell::UnsafeCell::new(core::mem::MaybeUninit::uninit()));
+
+/// Handler to have global allocator stored somewhere
+struct AllocatorHandler(core::cell::UnsafeCell<core::mem::MaybeUninit<GlobalAllocator>>);
+unsafe impl Sync for AllocatorHandler {}
+
+/// Initialize global allocator
+pub fn init_global_allocator(value: GlobalAllocator) {
+    let global_ref = unsafe { &mut *_GLOBAL_DEFAULT_EXTERNAL_ALLOCATOR.0.get() };
+    *global_ref = core::mem::MaybeUninit::new(value);
+}
+
+/// Get current global allocator. `init_global_allocator` must be called before
+pub fn get_global_allocator() -> GlobalAllocator {
+    unsafe { *(*_GLOBAL_DEFAULT_EXTERNAL_ALLOCATOR.0.get()).as_ptr() }
+}
+
 pub mod allocator;
 pub mod box_type;
 pub mod memory;
