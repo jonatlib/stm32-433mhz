d7611c07083888a746914c797193da6c66b626be
Libor Jonat <libor.jonat@quantlane.com>
2023-06-07 10:08:18 +0200

    WIP shared

--- workspaces/physical_layer/src/utils.rs
+++ workspaces/physical_layer/src/utils.rs
@@ -1,16 +1,22 @@
-use core::borrow::Borrow;
-use core::cell::{Ref, RefCell};
-use core::ops::Deref;
+use core::cell::{Ref, RefCell, RefMut};
 use static_cell::StaticCell;
 
 pub struct Shared<T: 'static> {
-    reference: &'static T,
+    reference: &'static RefCell<T>,
 }
 
 impl<T: 'static> Shared<T> {
-    pub fn new(value: T, memory: &'static StaticCell<T>) -> Self {
-        let reference: &'static T = memory.init(value);
+    pub fn new(value: T, memory: &'static StaticCell<RefCell<T>>) -> Self {
+        let reference: &'static RefCell<T> = memory.init(RefCell::new(value));
         Self { reference }
+    }
+
+    pub fn borrow(&self) -> Ref<'_, T> {
+        RefCell::borrow(self.reference)
+    }
+
+    pub fn borrow_mut(&self) -> RefMut<'_, T> {
+        RefCell::borrow_mut(self.reference)
     }
 }
 
