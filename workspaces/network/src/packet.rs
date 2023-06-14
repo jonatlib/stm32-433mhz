54e6d4d7e5a5b653d15386a72bf417f4708ffe6f
Libor Jonat <jonat.libor@gmail.com>
2023-06-14 11:45:52 +0200

    Preparing Packet64

--- workspaces/network/src/packet.rs
+++ workspaces/network/src/packet.rs
@@ -65,3 +65,52 @@ impl defmt::Format for Packet32 {
         )
     }
 }
+
+#[bitfield(u64)]
+pub struct Packet64 {
+    #[bits(2)]
+    pub kind: PacketKind,
+    #[bits(5)] // Up to 32 packets
+    pub sequence_number: SequenceNumber<32>,
+
+    #[bits(5)] // Up to 32 devices
+    pub source_address: u8,
+    #[bits(5)]
+    pub destination_address: u8,
+
+    #[bits(40)] // 5bytes
+    pub payload: u16,
+
+    #[bits(3)]
+    pub payload_used_index: u8,
+
+    #[bits(4)]
+    pub crc4: u8,
+}
+
+impl Packet64 {
+    pub fn update_crc(&mut self) {
+        todo!()
+    }
+}
+
+impl defmt::Format for Packet64 {
+    fn format(&self, fmt: defmt::Formatter) {
+        let payload = self.payload().to_be_bytes();
+        defmt::write!(
+            fmt,
+            "Packet64 {{ kind: {:?}, sequence_number: {}, source_address: {:#04x}, destination_address: {:#04x}, payload: [{:#04x}, {:#04x}, {:#04x}, {:#04x}, {:#04x}], payload_used_index: {}, crc4: {:#04x} }}",
+            self.kind(),
+            self.sequence_number(),
+            self.source_address(),
+            self.destination_address(),
+            payload[0],
+            payload[1],
+            payload[2],
+            payload[3],
+            payload[4],
+            self.payload_used_index(),
+            self.crc4(),
+        )
+    }
+}
