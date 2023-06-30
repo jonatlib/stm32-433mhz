5506a161cea7d948a46ff55158e8bf5ccf69378f
Libor Jonat <libor.jonat@quantlane.com>
2023-06-30 15:52:40 +0200

    Added full tests for networking

--- workspaces/network/src/tests.rs
+++ workspaces/network/src/tests.rs
@@ -6,7 +6,10 @@ use codec::{Codec, CodecSize, Identity};
 use crate::tests::network::{ReaderFactory, WriterFactory};
 use async_std::task::block_on;
 use async_std_test::async_test;
+use codec::chain::Chain;
+use codec::four_to_six::FourToSixBits;
 use codec::lzss::LzssCompression;
+use codec::reed_solomon::ReedSolomon;
 use std::future::Future;
 use std::vec::Vec;
 
@@ -57,43 +60,59 @@ where
     block_on(callback(transport_reader_factory, transport_writer_factory))
 }
 
-pub fn test_configuration<Cod, Com>()
-where
-    Cod: Codec + Default,
-    Com: Codec + Default,
-{
-    test_network(
-        // FIXME how to use `Cod` here?
-        |mut reader_factory: ReaderFactory<Identity, Com>, mut writer_factory| async move {
-            let mut reader = reader_factory.create_reader();
-            let mut writer = writer_factory.create_writer();
-            let payload = vec![0x01u8, 0x02, 0x03, 0x04, 0xff, 0xfe, 0xfd, 0xfc, 0xaa];
+macro_rules! test_configuration {
+    ($codec:ty, $compression: ty) => {
+        test_network(
+            // FIXME how to use `Cod` here?
+            |mut reader_factory, mut writer_factory: WriterFactory<$codec, $compression>| async move {
+                let mut reader = reader_factory.create_reader();
+                let mut writer = writer_factory.create_writer();
+                let payload = vec![0x01u8, 0x02, 0x03, 0x04, 0xff, 0xfe, 0xfd, 0xfc, 0xaa];
 
-            let wrote_bytes = writer
-                .send_bytes(&payload[..])
-                .await
-                .expect("Can't send data");
-            // FIXME assert for writing won't work
+                let wrote_bytes = writer
+                    .send_bytes(&payload[..])
+                    .await
+                    .expect("Can't send data");
+                // FIXME assert for writing won't work
 
-            let mut read_buffer = [0x00u8; 9];
-            let read_bytes = reader
-                .receive_bytes(&mut read_buffer)
-                .await
-                .expect("Can't receive data");
+                let mut read_buffer = [0x00u8; 9];
+                let read_bytes = reader
+                    .receive_bytes(&mut read_buffer)
+                    .await
+                    .expect("Can't receive data");
 
-            //TODO read bytes assert
-            println!("Read = {:?}", read_buffer);
-            assert_eq!(payload, Vec::from(read_buffer));
-        },
-    );
+                //TODO read bytes assert
+                println!("Read = {:?}", read_buffer);
+                assert_eq!(payload, Vec::from(read_buffer));
+            },
+        );
+    };
 }
 
 #[test]
 fn test_full_receive_transmit_identity() {
-    test_configuration::<Identity, Identity>();
+    test_configuration!(Identity, Identity);
 }
 
 #[test]
-fn test_full_receive_transmit_lzmo_compression() {
-    test_configuration::<Identity, LzssCompression>();
+fn test_full_receive_transmit_codec_reed_solomon() {
+    test_configuration!(ReedSolomon<4, 8>, Identity);
+}
+
+#[test]
+fn test_full_receive_transmit_codec_chain() {
+    test_configuration!(Chain<ReedSolomon<4, 8>, FourToSixBits<20>, 8>, Identity);
+}
+
+#[test]
+fn test_full_receive_transmit_lzss_compression() {
+    test_configuration!(Identity, LzssCompression);
+}
+
+#[test]
+fn test_full_receive_transmit_codec_complex_compression() {
+    test_configuration!(
+        Chain<ReedSolomon<4, 8>, FourToSixBits<20>, 8>,
+        LzssCompression
+    );
 }
